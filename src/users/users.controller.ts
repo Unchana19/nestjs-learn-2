ab43cf6835c48644052c7d65f6da6500388dfd92
annchana <annchanaoat@gmail.com>
2024-09-13 01:27:02 +0700

    dto

---
+++ src/users/users.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Headers,
+  Ip,
+  ParseIntPipe,
+  DefaultValuePipe,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get('/:id?')
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log(limit);
+    console.log(page);
+    return 'You sent a get request';
+  }
+
+  @Post()
+  public createUsers(
+    @Body() request: any,
+    @Headers() headers: any,
+    @Ip() ip: any,
+  ) {
+    console.log(request);
+    console.log(headers);
+    console.log(ip);
+    return 'You sent a post request';
+  }
+}
