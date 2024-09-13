b4cb441c775cc340b09071ea5e436fb3f1344f18
annchana <annchanaoat@gmail.com>
2024-09-13 14:38:14 +0700

    patch dto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,34 +5,36 @@ import {
   Param,
   Post,
   Query,
-  Headers,
-  Ip,
   ParseIntPipe,
   DefaultValuePipe,
+  Patch,
 } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUsersParamDto } from './dtos/get-users-param.dto';
+import { PatchUserDto } from './dtos/patch-user.dto';
 
 @Controller('users')
 export class UsersController {
   @Get('/:id?')
   public getUsers(
-    @Param('id', ParseIntPipe) id: number | undefined,
+    @Param() getUsersParamDto: GetUsersParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
+    console.log(getUsersParamDto);
     console.log(limit);
     console.log(page);
     return 'You sent a get request';
   }
 
   @Post()
-  public createUsers(
-    @Body() request: any,
-    @Headers() headers: any,
-    @Ip() ip: any,
-  ) {
-    console.log(request);
-    console.log(headers);
-    console.log(ip);
+  public createUsers(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto instanceof CreateUserDto);
     return 'You sent a post request';
+  }
+
+  @Patch()
+  public patchUser(@Body() patchUserDto: PatchUserDto) {
+    return patchUserDto;
   }
 }
