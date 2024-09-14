bf137a734b312de3dbe10038ea10d58548b02092
annchana <annchanaoat@gmail.com>
2024-09-15 00:52:33 +0700

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,6 +13,18 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJs Materclass - Blog app API')
+    .setDescription('Use The base API URL as http://localhost:3000')
+    .setTermsOfService('http://localhost:3000/term-of-service')
+    .setLicense('MIT License', '')
+    .addServer('http://localhost:3000')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
